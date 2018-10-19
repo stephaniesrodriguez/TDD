32765867f96396c83a4bbda7eeb958c2e23a0d0b
Stephaniesrodriguez <stephaniestreun@yahoo.com>
2018-10-18 23:36:34 -0500

    chapter3

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-home_page = None
 # Create your views here.
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title>')
